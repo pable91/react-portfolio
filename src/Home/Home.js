bfc124510e61553d0f81405d6a597155833a6c54
KimYongKwon <dydrnjs518@gmail.com>
2020-09-01 00:12:13 +0900

    home background animation

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="background"></div>
+        <div className="home__contents">hello</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
