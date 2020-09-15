424e1d09b6d836c8939ea0f81622d73288b1af94
KimYongKwon <dydrnjs518@gmail.com>
2020-09-15 16:28:03 +0900

    update about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./About.css";
 import myImg from "./YongKwon.jpg";
+import es6 from "./../image/es6.jpg";
+import babel from "./../image/babel.png";
+import pug from "./../image/pug.png";
 
 class About extends React.Component {
   render() {
@@ -13,21 +16,28 @@ class About extends React.Component {
             <div className="about__contents__who__text">
               <ul>
                 <li>
-                  사랑<hr></hr>
+                  책임감<hr></hr>
                 </li>
                 <li>
-                  믿음<hr></hr>
+                  호기심<hr></hr>
                 </li>
                 <li>
-                  소망<hr></hr>
-                </li>
-                <li>
-                  희망<hr className="lastHR"></hr>
+                  긍정<hr className="lastHR"></hr>
                 </li>
               </ul>
               <div className="about__contents__who__text__intruduce">
-                adadsadadasdddddddddddddddddddddddddddddddddddd
-                ddddddddddddddddddddddddddddddddddddddasdasdadadsa
+                안녕하세요.
+                <br></br>
+                <br></br>프론트엔드 개발자를 지원하게 된 김용권이라고 합니다.
+                나만의 포트폴리오 웹사이트를 만들어보자라는 작은 동기로 웹
+                프로그래밍 세계에 들어오게 되었습니다.
+                <br></br>
+                <br></br>많이 부족하지만 ( 맡은일에 대한 책임감 ), ( 개발에 대한
+                호기심 ), ( 매사의 긍정적인 마인드 )라는 저만의 장점을 활용해 더
+                실력있는 개발자가 되고 싶습니다.
+                <br></br>
+                <br></br>
+                감사합니다.
               </div>
             </div>
           </article>
@@ -49,7 +59,7 @@ class About extends React.Component {
                   <span>Javascript</span>
                 </li>
                 <li>
-                  <img src="/image/es6.jpg" />
+                  <img src={es6} />
                   <span>es6</span>
                 </li>
                 <li>
@@ -57,19 +67,21 @@ class About extends React.Component {
                   <span>React</span>
                 </li>
                 <li>
-                  <img src="/image/babel.png" />
+                  <img src={babel} />
                   <span>Babel</span>
                 </li>
                 <li>
-                  <img src="/image/pug.png" />
+                  <img src={pug} />
                   <span>pug</span>
                 </li>
               </ul>
               <div className="about__contents__skill__stack">backend</div>
               <ul className="about__contents__skill__list">
                 <li>
-                  <img src="https://img.icons8.com/color/96/000000/nodejs.png" />
-                  <span>node.js(express)</span>
+                  <div className="icon">
+                    <img src="https://img.icons8.com/color/96/000000/nodejs.png" />
+                  </div>
+                  <span>node.js</span>
                 </li>
                 <li>
                   <img src="https://img.icons8.com/color/96/000000/mongodb.png" />
