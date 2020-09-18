73063df49e17c5aff067c68bb3d26341d8eac89c
KimYongKwon <dydrnjs518@gmail.com>
2020-09-19 00:00:36 +0900

    Update project section

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -16,8 +16,14 @@ class Project extends React.Component {
               <span style={{ color: "rgb(209, 180, 140, 1)" }}>HTML, CSS</span>
             </div>
             <div className="introduce">
-              <h3>Project1</h3>
-              <p>dasdsassssssssssssssssssssssssssssssssssssssssssssssss</p>
+              <h3>Unknown Webpage</h3>
+              <p>
+                처음 만들어본 웹페이지로서, 구글링으로 찾은 임의의 웹페이지를
+                클론코딩 하였습니다. 본 프로젝트를 통해 기본적인 HTML, CSS를
+                익히게 되었습니다.
+              </p>
+              <div className="introduce__link">LIVE</div>
+              <div className="introduce__link">CODE</div>
             </div>
           </div>
           <div className="project__contents__object">
@@ -30,11 +36,13 @@ class Project extends React.Component {
               <span style={{ color: "rgb(209, 180, 140, 1)" }}>HTML, CSS</span>
             </div>
             <div className="introduce">
-              <h3>Project2</h3>
+              <h3>Momentum App</h3>
               <p>
-                dasdsa dsadasdasdasddasdasdasdasdasdasdsadsadasdas
-                asdasdasdasdasdasdasdasdasdasdasdasdasdasdas
+                Momentum이라는 앱을 비슷하게 만들어봤습니다. 이 프로젝트를 통해
+                javaScript를 활용한 동적인 프로그래밍이 가능해졌습니다.
               </p>
+              <div className="introduce__link">LIVE</div>
+              <div className="introduce__link">CODE</div>
             </div>
           </div>
           <div className="project__contents__object">
@@ -47,10 +55,10 @@ class Project extends React.Component {
               <span style={{ color: "rgb(209, 180, 140, 1)" }}>React</span>
             </div>
             <div className="introduce">
-              <h3>Project3</h3>
+              <h3>Movie App</h3>
               <p>
-                dasdsa dsadasdasdasddasdasdasdasdasdasdsadsadasdas
-                asdasdasdasdasdasdasdasdasdasdasdasdasdasdas
+                React.js를 사용한 첫 프로젝트로, 영화목록 데이터를 받아와서
+                정렬하고 영화조회가 가능한 프로젝트입니다.
               </p>
             </div>
           </div>
@@ -63,18 +71,15 @@ class Project extends React.Component {
               }}
             >
               <span style={{ color: "rgb(209, 180, 140, 1)" }}>
-                JAVA SCRIPT<br></br>FULL STACK
+                NodeJS<br></br>FULL STACK
               </span>
             </div>
             <div className="introduce">
-              <h3>Project4</h3>
-              <p>
-                dasdsa dsadasdasdasddasdasdasdasdasdasdsadsadasdas
-                asdasdasdasdasdasdasdasdasdasdasdasdasdasdas
-              </p>
+              <h3>ktube</h3>
+              <p>NodeJS(epxress)를 사용해서 유튜브를 클론코딩하였습니다.</p>
             </div>
           </div>
-          <div className="project__contents__object">
+          {/* <div className="project__contents__object">
             <div
               className="subject"
               style={{
@@ -92,8 +97,8 @@ class Project extends React.Component {
                 asdasdasdasdasdasdasdasdasdasdasdasdasdasdas
               </p>
             </div>
-          </div>
-          <div className="project__contents__object">
+          </div> */}
+          {/* <div className="project__contents__object">
             <div
               className="subject"
               style={{
@@ -111,7 +116,7 @@ class Project extends React.Component {
                 asdasdasdasdasdasdasdasdasdasdasdasdasdasdas
               </p>
             </div>
-          </div>
+          </div> */}
         </section>
       </div>
     );
